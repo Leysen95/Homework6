f0816bb12b124c72c42187b14d854135c3dc6aa6
Leysen95 <mullayanova95@bk.ru>
2024-06-10 19:54:12 +0500

    6, 8, 9 changed

--- src/Main.java
+++ src/Main.java
@@ -5,50 +5,68 @@ public class Main {
             System.out.print(i + " ");
         }
         System.out.println();
+
         System.out.println("Task 2");
         int i = 10;
         for (; i >= 1; i--) {
             System.out.print(i + " ");
         }
         System.out.println();
+
         System.out.println("Task 3");
         i = 0;
         for (; i <= 17; i = i + 2) {
                 System.out.print(i + " ");
         }
         System.out.println();
+
         System.out.println("Task 4");
         i = 10;
         for (; i >= -10; i--) {
             System.out.print(i +  " ");
         }
         System.out.println();
+
         System.out.println("Task 5");
         i = 1904;
         for (; i <= 2096; i = i + 4) {
             System.out.println(i + " год является високосным");
         }
+
         System.out.println("Task 6");
-        i = 0;
+        i = 7;
         for (; i <= 100; i = i + 7) {
             System.out.print(i + " ");
         }
         System.out.println();
+
         System.out.println("Task 7");
         i = 1;
         for (; i <= 600; i = i * 2) {
             System.out.print(i + " ");
         }
         System.out.println();
-        System.out.println("Task 8-9");
+
+        System.out.println("Task 8");
         double total = 0;
         int deposite = 29000;
-        double percentPerMonth = 1.01;
         for (int month = 1; month <= 12; month++) {
 total += deposite;
-total *= percentPerMonth;
-            System.out.printf("Месяц %s, сумма накоплений равна %s рублей%n", month, total );
+            System.out.printf("Месяц %s, сумма накоплений равна %.2f рублей%n", month, total );
         }
+        System.out.println();
+
+        System.out.println("Task 9");
+        total = 0;
+        deposite = 29000;
+        double percentPerMonth = 1.01;
+        for (int month = 1; month <= 12; month++) {
+            total += deposite;
+            total *= percentPerMonth;
+            System.out.printf("Месяц %s, сумма накоплений равна %.2f рублей%n", month, total );
+        }
+        System.out.println();
+
         System.out.println("Task 10");
 for (i = 1; i <= 10; i++) {
     System.out.printf("2 * %s = %s%n", i, i * 2);
